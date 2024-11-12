300f4dc2a195405e2f8ddef651315b1e1b2dae15
Rodri34451 <rodrigo-1952001@live.com.ar>
2024-11-12 18:50:15 -0300

    first commit

---
+++ WebEcuelaJson/Default.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CapaDeNegocio;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request["accion"] == null) return;
+        switch (Request["accion"])
+        {
+
+            case "ADDUSUARIO": AddUsuario(); break;
+            case "LISTUSUARIOS": ListUsuarios(); break;
+            case "DELETEUSER": DeleteUser(); break;
+            case "MODIFYUSER": ModifyUser(); break;
+            case "FINDUSUARIO": FindUser(); break;
+            default:
+                Response.Write("Invalid action");
+                break;
+        }
+    }
+
+    private void AddUsuario()
+    {
+        Usuario U = new Usuario();
+        U.Nombre = Request["Nombre"];
+        U.Mail = Request["Mail"];
+        U.Dni = int.Parse(Request["Dni"]);
+        try
+        {
+            U.Add();
+            Response.Write("OK");
+        }
+        catch (Exception er)
+        {
+            Response.Write(er.Message);
+        }
+
+    }
+    private void ListUsuarios()
+    {
+        Usuario U = new Usuario();
+        string lista = U.List();
+        Response.Write(lista);
+
+    }
+
+    private void DeleteUser()
+    {
+        Usuario U = new Usuario();
+        U.ID = int.Parse(Request["ID"]);
+
+        try
+        {
+            U.Erase();
+            Response.Write("OK");
+        }
+        catch (Exception er)
+        {
+            Response.Write(er.Message);
+        }
+    }
+
+    private void ModifyUser()
+    {
+        Usuario U = new Usuario();
+
+        U.ID = int.Parse(Request["ID"]);
+        U.Nombre = Request["Nombre"];
+        U.Dni = int.Parse(Request["Dni"]);
+        U.Mail = Request["Mail"];
+        try
+        {
+            U.Modify();
+            Response.Write("OK");
+        }
+        catch (Exception er)
+        {
+            Response.Write(er.Message);
+        }
+
+    }
+
+    private void FindUser()
+    {
+        Usuario U = new Usuario();
+        U.ID = int.Parse(Request["ID"]);
+
+        try
+        {
+            string user = U.Find();
+            Response.Write(user);
+        }
+        catch (Exception er)
+        { 
+            Response.Write(er.Message); 
+        }
+    }
+} 
+
---
+++ WebEcuelaJson/js/nav.js
@@ -0,0 +1,14 @@
+const $$Nav=function(){
+
+    this.init = () => {
+        $ds.nav("body");
+        $dn.makeButton("Inicio",$ds.home);
+        $dn.makeButton("Carrera",$f.addUser);
+        $dn.makeButtonLogin("Ingresar")
+        $dn.makeDropDown("Funciones",["abm usuario","Buscar Usuario"],[$f.addUser,$f.findUser]);
+
+    }
+
+}
+
+const $nav=new $$Nav
